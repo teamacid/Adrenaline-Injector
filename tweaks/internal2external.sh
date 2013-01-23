75f951de79408d97efe2646f2fbdbf4569ae6332
Daniel Sexton <dsexton702@gmail.com>
2013-01-23 12:17:18 -0800

    thanks bhundven!

--- tweaks/internal2external.sh
+++ tweaks/internal2external.sh
@@ -3,12 +3,8 @@
 #Internal2External script
 
 
-mount -o remount,rw /
+E=echo "$(grep 'vold' /proc/mounts | grep sdcard | cut -d' ' -f1)" 
 
-mount -t vfat -o umask=0000 /dev/block/vold/179:97 /storage/sdcard0
+mount -t vfat -o umask=0000 $E /storage/sdcard0
 
-mount -t exfat -o umask=0000 /dev/block/vold/179:97 /storage/sdcard0
-
-if busybox mount | busybox grep vold/179:97; then
-busybox mount -o bind /data/media /storage/sdcard1  
-fi
+mount -o bind /data/media /storage/sdcard1  
